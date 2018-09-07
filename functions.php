1bd7cd94cd6e89f694501d008ce1e463fbe684d2
Saysaii <krisu.97@hotmail.com>
2018-09-07 10:31:59 +0300

    footer improvements

--- functions.php
+++ functions.php
@@ -83,6 +83,7 @@ class Digiloikka extends Timber\Site {
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
 		$context['menu'] = new TimberMenu();
+		$context['footer_menu'] = new TimberMenu("nav-footer");
 		$context['site'] = $this;
 
 		return $context;
