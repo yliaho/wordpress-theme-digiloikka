c691c49d802b30161b99a641099b4220f430241d
Saysaii <krisu.97@hotmail.com>
2018-09-27 13:03:04 +0300

    backup test

--- src/components/ad-modal.ts
+++ src/components/ad-modal.ts
@@ -4,10 +4,16 @@ export default class Modal {
   private btn: HTMLElement
   private span: HTMLElement
 
+  private modal1: HTMLElement
+
+  private btn1: HTMLElement
+  private span1: HTMLElement
+
   constructor(private element: HTMLElement) {
     this.btn = this.element.querySelector('#myBtn')
     this.modal = this.element.querySelector('#myModal')
     this.span = this.element.querySelector('#close')
+
     this.click()
     console.log(this.element)
   }
