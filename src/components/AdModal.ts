40ba33df715d5a2cb1fd95621a71ed8ff4fbff96
Joonas Yliaho <joonas.yliaho@gmail.com>
2018-10-02 12:09:03 +0300

    adAgency cubes and modal

--- src/components/AdModal.ts
+++ src/components/AdModal.ts
@@ -1,12 +1,37 @@
 export default class AdModal {
   private modalVidContainer: HTMLElement = null
+  public modalVideoContent: HTMLElement = null
+  private modalCloseSpan: HTMLElement = null
 
   constructor(private element: HTMLElement) {
     this.modalVidContainer = this.element.querySelector('.modal-vid')
+    this.modalCloseSpan = this.element.querySelector('span.close')
+
+    // click on myModal
+    this.element.addEventListener('click', e => {
+      if (e.target !== this.element) return
+
+      this.hideEmbed()
+    })
+
+    // click on close span
+    this.modalCloseSpan.addEventListener('click', e => {
+      this.hideEmbed()
+    })
   }
 
   public setEmbed(embedString: string) {
     this.element.style.display = 'flex'
-    this.element.innerHTML = embedString
+    this.modalVideoContent = this.element.querySelector(
+      '.modal-content .video-content'
+    )
+    this.modalVideoContent.innerHTML = embedString
+  }
+
+  public hideEmbed() {
+    console.log('moi')
+    console.log(this)
+    this.modalVideoContent.innerHTML = ''
+    this.element.style.display = 'none'
   }
 }
