1f857b4baeb4bf09b6cec6946357f500fa432bc8
Saysaii <krisu.97@hotmail.com>
2018-10-16 16:26:18 +0300

    conditional logic for follow us text

--- src/components/MobileNav.ts
+++ src/components/MobileNav.ts
@@ -1,6 +1,10 @@
+import * as anime from 'animejs'
+
 export default class MobileNav {
+  private closeButtonEl: HTMLElement
   constructor(private element: HTMLElement, private navEl: HTMLElement) {
-    console.log('asdasd')
+    this.navEl.classList.add('inactive')
+    this.createCloseButton()
     this.element.addEventListener(
       'click',
       this.hamburgerClickListener.bind(this)
@@ -9,5 +13,31 @@ export default class MobileNav {
 
   public hamburgerClickListener(e: Event) {
     this.navEl.classList.toggle('active')
+    if (this.navEl.classList.contains('active')) {
+      this.openNav()
+    }
+  }
+  private openNav() {
+    this.navEl.classList.remove('inactive')
+    this.navEl.classList.add('active')
+    const fadeInMobileNav = anime({
+      targets: this.navEl,
+      left: ['100vw', '20vw'],
+      easing: 'easeOutCubic',
+      duration: 400
+    })
+  }
+  private closeNav() {
+    this.navEl.classList.remove('active')
+  }
+  private createCloseButton() {
+    this.closeButtonEl = document.createElement('div')
+    this.closeButtonEl.classList.add('close-button', 'fa', 'fa-times')
+    this.navEl.appendChild(this.closeButtonEl)
+
+    this.closeButtonEl.addEventListener('click', () => {
+      this.closeNav()
+      console.log('asasd')
+    })
   }
 }
