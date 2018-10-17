c00165a3b2314afce05a36c2dea643b80dd9ed1f
Saysaii <krisu.97@hotmail.com>
2018-10-17 13:09:19 +0300

    different nav menus for different languages

--- functions.php
+++ functions.php
@@ -100,7 +100,10 @@ class Digiloikka extends Timber\Site {
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
 		$context['menu'] = new TimberMenu("nav-menu");
+		$context['menu_en'] = new TimberMenu("nav-menu-en");
+		$context['menu_sv'] = new TimberMenu("nav-menu-sv");
 		$context['footer_menu'] = new TimberMenu("nav-footer");
+		$context['footer_menu_en'] = new TimberMenu("nav-footer-en");
 		$context['site'] = $this;
 
 		return $context;
