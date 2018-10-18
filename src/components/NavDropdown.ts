822caf098a5455d2c3abc2ead8e7c62dbe2a1f42
Joonas Yliaho <joonas.yliaho@gmail.com>
2018-10-18 12:43:21 +0300

    really bad desktop & mobile nav behaviour

--- src/components/NavDropdown.ts
+++ src/components/NavDropdown.ts
@@ -1,10 +1,17 @@
+import getWindowClientWidth from '../utils/getWindowClientWidth'
+
 export default class NavDropdown {
   private dropdown: HTMLTemplateElement
   private nub: HTMLElement
   private isOpen: boolean = false
   constructor(private element: HTMLElement) {
     const aTag = this.element.querySelector('a')
-    aTag.addEventListener('click', e => {
+    const mdBreakpoint = 1024
+
+    this.dropdown = this.element.querySelector('ul.child')
+    this.nub = this.element.parentElement.querySelector('.nub') as HTMLElement
+
+    const clickListener = e => {
       if (e.target === aTag) {
         e.preventDefault()
         e.stopPropagation()
@@ -16,11 +23,25 @@ export default class NavDropdown {
           this.isOpen = false
         }
       }
+    }
+
+    const toggleChild = () => {
+      if (getWindowClientWidth() <= mdBreakpoint) {
+        aTag.removeEventListener('click', clickListener)
+        this.mobileBehaviour()
+      } else if (getWindowClientWidth() > mdBreakpoint) {
+        aTag.addEventListener('click', clickListener)
+        this.desktopBehaviour()
+      }
+    }
+    toggleChild()
+
+    window.addEventListener('resize', () => {
+      toggleChild()
     })
   }
 
   public showDropdown() {
-    this.dropdown = this.element.querySelector('ul.child')
     this.dropdown.style.display = 'flex'
 
     const {
@@ -36,7 +57,7 @@ export default class NavDropdown {
     this.dropdown.style.left = `${Math.floor(elRight) - dropdownWidth}px`
 
     const nubWidth = 12
-    this.nub = this.element.parentElement.querySelector('.nub') as HTMLElement
+
     this.nub.style.borderWidth = `${nubWidth}px`
     this.nub.style.display = 'block'
     this.nub.style.top = `${elTop + 30 - nubWidth}px`
@@ -48,5 +69,25 @@ export default class NavDropdown {
     console.log('what')
     this.dropdown.style.display = 'none'
     this.nub.style.display = 'none'
+  }
+
+  private desktopBehaviour() {
+    if (
+      this.dropdown.classList.contains('mobile') &&
+      this.nub.classList.contains('mobile')
+    ) {
+      this.dropdown.classList.remove('mobile')
+      this.nub.classList.remove('mobile')
+    }
+  }
+
+  private mobileBehaviour() {
+    if (
+      !this.dropdown.classList.contains('mobile') &&
+      !this.nub.classList.contains('mobile')
+    ) {
+      this.dropdown.classList.add('mobile')
+      this.nub.classList.add('mobile')
+    }
   }
 }
