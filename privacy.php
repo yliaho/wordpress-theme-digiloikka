a712047910fa445f88383dcbd1ccb5b82aaab80a
Saysaii <krisu.97@hotmail.com>
2018-11-01 09:19:10 +0200

    added multilanguage fields for gdpr popup(old)

--- privacy.php
+++ privacy.php
@@ -11,5 +11,7 @@ $post = Timber::query_post();
 $context['post'] = $post;
 $context['stadi_logo'] = get_field('stadi_logo', 'options');
 $context['gdpr_popup'] = get_field('gdpr_popup', 'options');
+$context['gdpr_popup_en'] = get_field('gdpr_popup_en', 'options');
+$context['gdpr_popup_sv'] = get_field('gdpr_popup_sv', 'options');
 $templates = ['privacy-policy.twig'];
 Timber::render($templates, $context);
